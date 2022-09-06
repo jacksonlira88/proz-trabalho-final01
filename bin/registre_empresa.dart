e89ac47a3ac6799abbad742ae180ad649512498f
jacksonlira88 <jacksonlira.rs@gmail.com>
2022-09-06 18:43:03 -0300

    Projeto 01 - initial commit

---
+++ bin/registre_empresa.dart
@@ -0,0 +1,5 @@
+import 'package:registre_empresa/registre_empresa.dart' as registre_empresa;
+
+void main(List<String> arguments) {
+  print('Hello world: ${registre_empresa.calculate()}!');
+}
---
+++ test/registre_empresa_test.dart
@@ -0,0 +1,8 @@
+import 'package:registre_empresa/registre_empresa.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
