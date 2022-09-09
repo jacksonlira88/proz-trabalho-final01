5c3ffcd3b625d9d140fab1e36cd04842e7a74899
jacksonlira88 <jacksonlira.rs@gmail.com>
2022-09-08 23:27:50 -0300

    start: delega seviços e acesso a dados

---
+++ lib/repository/api_provider_empresas.dart
@@ -0,0 +1,101 @@
+import 'package:registre_empresa/model/empresa.dart';
+import 'package:registre_empresa/model/pessoa_fisica.dart';
+import 'package:registre_empresa/model/pessoa_juridica.dart';
+
+class APIProviderEmpresas {
+  late final List<Empresa> _empresasCadastradas = [];
+  late final List<PessoaFisica> _sociosPF = [];
+  late final List<PessoaJuridica> _sociosPJ = [];
+
+//metodos para pessoa juridica
+  bool cadastrarSocioPJ(PessoaJuridica socio) {
+    _sociosPJ.add(socio);
+    return true;
+  }
+
+  List<PessoaJuridica> listarSociosPJ() {
+    return _sociosPJ;
+  }
+
+  PessoaJuridica? getSocioPJId(String id) {
+    for (var element in _sociosPJ) {
+      if (element.id == id) {
+        return element;
+      }
+    }
+    return null;
+  }
+
+// métodos para sócio pessoa física
+  bool cadastrarSocioPF(PessoaFisica socio) {
+    _sociosPF.add(socio);
+    return true;
+  }
+
+  List<PessoaFisica> listarSociosPF() {
+    return _sociosPF;
+  }
+
+  PessoaFisica? getSocioPFId(String id) {
+    PessoaFisica? pf;
+    for (var element in _sociosPF) {
+      if (element.id == id) {
+        pf = element;
+      } else {
+        pf = null;
+      }
+    }
+    return pf;
+  }
+
+// metodos para empresa
+  Empresa? getEmpresaCPFSocio(String cpfSocio) {
+    for (var element in _empresasCadastradas) {
+      if (element.cnpj == cpfSocio) {
+        return element;
+      }
+    }
+    return null;
+  }
+
+// se empresa não tiver sócio com esse cnpj retorna null
+  Empresa? getEmpresaCNPJSocio(String cnpjSocio) {
+    for (var socio in _sociosPJ) {
+      for (var empresa in _empresasCadastradas) {
+        if (socio.cnpj == cnpjSocio) {
+          if (empresa.id == socio.id) {
+            return empresa;
+          }
+        }
+      }
+    }
+    return null;
+  }
+
+  List<Empresa> listarEmpresa() {
+    return _empresasCadastradas;
+  }
+
+  bool cadastrarEmpresa(Empresa empresa) {
+    _empresasCadastradas.add(empresa);
+    return true;
+  }
+
+  bool removerEmpresa(String id) {
+    if (id != '') {
+      if (_empresasCadastradas.isEmpty) {
+        return false;
+      } else {
+        for (var element in _empresasCadastradas) {
+          element.id = id;
+          bool result = _empresasCadastradas.remove(element);
+          if (result) {
+            return true;
+          }
+          return false;
+        }
+      }
+    }
+    return false;
+  }
+}
---
+++ lib/repository/repository_empresas.dart
@@ -0,0 +1,55 @@
+// ignore: file_names
+import 'package:registre_empresa/model/pessoa_fisica.dart';
+import 'package:registre_empresa/model/pessoa_juridica.dart';
+import 'package:registre_empresa/repository/api_provider_empresas.dart';
+import 'package:registre_empresa/model/empresa.dart';
+
+class RepositoryEmpresas {
+  late final APIProviderEmpresas _apiProvider;
+  RepositoryEmpresas() {
+    _apiProvider = APIProviderEmpresas();
+  }
+
+  bool cadastrarSocioPF(PessoaFisica socio) {
+    return _apiProvider.cadastrarSocioPF(socio);
+  }
+
+  PessoaFisica? getSocioPFId(String id) {
+    return _apiProvider.getSocioPFId(id);
+  }
+
+  bool cadastrarSocioPJ(PessoaJuridica socio) {
+    return _apiProvider.cadastrarSocioPJ(socio);
+  }
+
+  PessoaJuridica? getSocioPJId(String id) {
+    return _apiProvider.getSocioPJId(id);
+  }
+
+// metodos para  empresa
+
+  bool cadastrarEmpresa(Empresa empresa) {
+    return _apiProvider.cadastrarEmpresa(empresa);
+  }
+
+  bool removerEmpresa(String id) {
+    return _apiProvider.removerEmpresa(id);
+  }
+
+  List<Empresa> listaEmpresas() {
+    return _apiProvider.listarEmpresa();
+  }
+
+// não concluido
+  String getEmpresaCNPJ(String cnpj) {
+    return 'Empresa';
+  }
+
+  Empresa? getEmpresaCNPJSocio(String cnpj) {
+    return _apiProvider.getEmpresaCNPJSocio(cnpj);
+  }
+
+  String getEmpresaCPFSocio(String cpf) {
+    return 'Empresa';
+  }
+}
